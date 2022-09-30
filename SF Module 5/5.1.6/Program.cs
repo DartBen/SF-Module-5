964e81592341df3d53677a7d438393c7813b31aa
Dmitry <ryabchikk@rambler.ru>
2022-09-30 19:51:49 +0300

    5.1.6

---
+++ SF Module 5/5.1.6/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var arr = GetArrayFromConsole();
+
+        Console.Write("Сортированный массив:");
+        foreach(var item in arr)
+        {
+            Console.Write(item);
+        }
+        Console.WriteLine();
+    }
+
+    static int[] GetArrayFromConsole()
+    {
+        var result = new int[5];
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+            result[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(result);
+
+
+        return result;
+    }
+}
