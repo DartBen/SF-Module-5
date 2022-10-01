b41795415244cc509fad03eeddceeeac51e4f5d6
Dmitry <ryabchikk@rambler.ru>
2022-10-01 14:59:20 +0300

    5.3.13
    Signed-off-by: Dmitry <ryabchikk@rambler.ru>

--- SF Module 5/5.3.11/Program.cs
+++ SF Module 5/5.3.11/Program.cs
@@ -1,2 +1,177 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        (string name, int age, string[] FavoriteColors) User;
+
+        WriteLine("Введите имя:");
+        User.name = ReadLine();
+        WriteLine("Введите возраст:");
+        int.TryParse(ReadLine(), out User.age);
+
+
+        //User.FavoriteColors = new string[3];
+
+        //Console.WriteLine("Введите три любимых цвета пользователя");
+        //for (int i = 0; i < User.FavoriteColors.Length; i++)
+        //{
+        //    User.FavoriteColors[i] = ShowColor(User.name, User.age);
+        //}
+
+        //WriteLine("Ваши любимые цвета:");
+        //foreach (var color in User.FavoriteColors)
+        //{
+        //    WriteLine(color);
+        //}
+
+        int tmp = Convert.ToInt32(args[0]);
+        var arr = GetArrayFromConsole(ref tmp);
+        var sortedarray = CustomSort(arr);
+
+        //Console.Write("Сортированный массив:");
+        //foreach (var item in sortedarray)
+        //{
+        //    Console.Write(item);
+        //}
+        //Console.WriteLine();
+
+        //ShowArray(arr, true);
+
+        ChangeAge(ref User.age);
+
+        Write(User.age);
+
+
+    }
+
+    static int[] GetArrayFromConsole(ref int num)
+    {
+        var result = new int[num];
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+            result[i] = int.Parse(Console.ReadLine());
+        }
+
+        //        Array.Sort(result);
+        //        result = CustomSort(result);
+
+        return result;
+    }
+
+    static int[] CustomSort(int[] arr)
+    {
+        int[] result = new int[arr.Length];
+        Array.Copy(arr, result, arr.Length);
+
+        int temp;
+
+        for (int y = 0; y < result.Length - 1; y++)
+        {
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+
+                if (result[i] > result[i + 1])
+                {
+                    temp = result[i];
+                    result[i] = result[i + 1];
+                    result[i + 1] = temp;
+                };
+            }
+        }
+        return result;
+    }
+
+    static string GetDataFromConsole() => Console.ReadLine();
+    static string ShowColor(string name, int age)
+    {
+        Console.WriteLine("{0},{1} \nНапишите свой любимый цвет на английском с маленькой буквы", name, age);
+        var color = GetDataFromConsole();
+
+        switch (color)
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Your color is yellow!");
+                break;
+        }
+
+        return color;
+    }
+
+    static void SortComplexArray(int[,] arr)
+    {
+        int temp;
+        for (int i = 0; i <= arr.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                    if (arr[i, j] > arr[i, k])
+                    {
+                        temp = arr[i, k];
+                        arr[i, k] = arr[i, j];
+                        arr[i, j] = temp;
+                    }
+        }
+    }
+
+    static void ShowArray(int[] arr, bool needsort = false)
+    {
+        int[] tempArr = new int[arr.Length];
+
+        Array.Copy(arr, tempArr, arr.Length);
+
+        if (needsort)
+        {
+            tempArr = CustomSort(tempArr);
+        }
+
+        Write("Сортированный массив методом ShowArray:");
+        foreach (var a in tempArr)
+        {
+            Write(" " + a + " ");
+        }
+        WriteLine();
+
+    }
+
+    static void ChangeAge(ref int age)
+    {
+        WriteLine("Изменение возраста в кортеже:");
+        age = Int32.Parse(ReadLine());
+    }
+
+    static int SumNumbers(ref int num1, in int num2, out int num3, int num4)
+    {
+        int result = 0;
+
+
+        num3 = 0;
+        return result;
+    }
+}
--- SF Module 5/5.3.13/Program.cs
+++ SF Module 5/5.3.13/Program.cs
@@ -1,2 +1,233 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        (string name, int age, string[] FavoriteColors) User;
+
+        WriteLine("Введите имя:");
+        User.name = ReadLine();
+        WriteLine("Введите возраст:");
+        int.TryParse(ReadLine(), out User.age);
+
+
+        int tmp = Convert.ToInt32(args[0]);
+        var arr = GetArrayFromConsole(ref tmp);
+        //var sortedarray = CustomSort(arr);
+
+        //ChangeAge(ref User.age);
+
+        //Write(User.age);
+
+
+        SortArray(in arr, out var arrO1, out var arrO2);
+
+        WriteLine("asc: ");
+        foreach (var item in arrO1)
+        {
+            Write(item+" ");
+        }
+
+        WriteLine("\ndesc");
+        foreach(var item in arrO2)
+        {
+            Write(item+" ");
+        }
+
+    }
+
+    static int[] GetArrayFromConsole(ref int num)
+    {
+        var result = new int[num];
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+            result[i] = int.Parse(Console.ReadLine());
+        }
+
+        //        Array.Sort(result);
+        //        result = CustomSort(result);
+
+        return result;
+    }
+
+    static int[] CustomSort(int[] arr)
+    {
+        int[] result = new int[arr.Length];
+        Array.Copy(arr, result, arr.Length);
+
+        int temp;
+
+        for (int y = 0; y < result.Length - 1; y++)
+        {
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+
+                if (result[i] > result[i + 1])
+                {
+                    temp = result[i];
+                    result[i] = result[i + 1];
+                    result[i + 1] = temp;
+                };
+            }
+        }
+        return result;
+    }
+
+    static void SortArray(in int[] arrIn, out int[] arrAsc, out int[] arrDesc)
+    {
+        arrAsc = new int[arrIn.Length];
+        arrDesc = new int[arrIn.Length];
+
+        SortArrayDesc(arrIn, out arrDesc);
+        SortArrayAsc(arrIn, out arrAsc);
+    }
+
+    /// <summary>
+    /// Сортировка по уменьшению
+    /// </summary>
+    /// <param name="arrIn"></param>
+    /// <param name="arrResult"></param>
+    static void SortArrayDesc(in int[] arrIn, out int[] arrResult)
+    {
+        int temp;
+        int[] arrTmp = new int[arrIn.Length];
+        Array.Copy(arrIn, arrTmp, arrIn.Length);
+
+        for (int y = 0; y < arrTmp.Length - 1; y++)
+        {
+            for (int i = 0; i < arrTmp.Length - 1; i++)
+            {
+
+                if (arrTmp[i] < arrTmp[i + 1])
+                {
+                    temp = arrTmp[i];
+                    arrTmp[i] = arrTmp[i + 1];
+                    arrTmp[i + 1] = temp;
+                };
+            }
+        }
+        arrResult = arrTmp;
+    }
+
+    /// <summary>
+    /// Сортировка во возрастанию
+    /// </summary>
+    /// <param name="arrIn"></param>
+    /// <param name="arrResult"></param>
+    static void SortArrayAsc(in int[] arrIn, out int[] arrResult)
+    {
+        int temp;
+        int[] arrTmp = new int[arrIn.Length];
+        Array.Copy(arrIn, arrTmp, arrIn.Length);
+
+        for (int y = 0; y < arrTmp.Length - 1; y++)
+        {
+            for (int i = 0; i < arrTmp.Length - 1; i++)
+            {
+
+                if (arrTmp[i] > arrTmp[i + 1])
+                {
+                    temp = arrTmp[i];
+                    arrTmp[i] = arrTmp[i + 1];
+                    arrTmp[i + 1] = temp;
+                };
+            }
+        }
+        arrResult = arrTmp;
+    }
+
+    static string GetDataFromConsole() => Console.ReadLine();
+    static string ShowColor(string name, int age)
+    {
+        Console.WriteLine("{0},{1} \nНапишите свой любимый цвет на английском с маленькой буквы", name, age);
+        var color = GetDataFromConsole();
+
+        switch (color)
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Your color is yellow!");
+                break;
+        }
+
+        return color;
+    }
+
+    static void SortComplexArray(int[,] arr)
+    {
+        int temp;
+        for (int i = 0; i <= arr.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                    if (arr[i, j] > arr[i, k])
+                    {
+                        temp = arr[i, k];
+                        arr[i, k] = arr[i, j];
+                        arr[i, j] = temp;
+                    }
+        }
+    }
+
+    static void ShowArray(int[] arr, bool needsort = false)
+    {
+        int[] tempArr = new int[arr.Length];
+
+        Array.Copy(arr, tempArr, arr.Length);
+
+        if (needsort)
+        {
+            tempArr = CustomSort(tempArr);
+        }
+
+        Write("Сортированный массив методом ShowArray:");
+        foreach (var a in tempArr)
+        {
+            Write(" " + a + " ");
+        }
+        WriteLine();
+
+    }
+
+    static void ChangeAge(ref int age)
+    {
+        WriteLine("Изменение возраста в кортеже:");
+        age = Int32.Parse(ReadLine());
+    }
+
+    static int SumNumbers(ref int num1, in int num2, out int num3, int num4)
+    {
+        int result = 0;
+
+
+        num3 = 0;
+        return result;
+    }
+
+
+}
